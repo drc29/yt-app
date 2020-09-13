494fc73d0d5ba447f0dab1b6e7505cd27f32a8df
Dustin Red Cabatay <dustinredcabatay@gmail.com>
2020-09-13 13:02:45 +0800

    Updates

---
+++ precache-manifest.9bcb8018312b81ddd14cb2b4a290f406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb3d8df5e3779f30d45982f5071fb01",
+    "url": "/yt-app/index.html"
+  },
+  {
+    "revision": "460f661472eda076cded",
+    "url": "/yt-app/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "1c6eb35215a2acd0fea0",
+    "url": "/yt-app/static/js/2.39511dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yt-app/static/js/2.39511dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460f661472eda076cded",
+    "url": "/yt-app/static/js/main.db96e39e.chunk.js"
+  },
+  {
+    "revision": "889ebda52d9efcf96752",
+    "url": "/yt-app/static/js/runtime-main.63c215d9.js"
+  }
+]);
